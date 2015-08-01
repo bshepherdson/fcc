f2f98f6253b1ae63f11f7cafe4dc72b6e0e271d6
Braden Shepherdson <braden.shepherdson@gmail.com>
2015-08-01 12:19:36 -0400

    Implemented DEFER IS DEFER@ DEFER! and ACTION-OF

--- stage0/lib/ext.fs
+++ stage0/lib/ext.fs
@@ -1,5 +1,7 @@
 \ CORE EXT words, selectively.
 
+: cr 10 emit ;
+
 : .(   [char] ) parse type ; IMMEDIATE
 
 : <> = INVERT ;
@@ -14,6 +16,29 @@
   0 , \ Placeholder for the jump offset to go.
 ; IMMEDIATE
 
+
+\ DEFER and friends
+\ A DEFERred word is a cell whose value contains an xt. When the word is
+\ called, its xt will be executed.
+: DEFER create 0 , DOES> @ execute ;
+
+: ACTION-OF
+  BL word find ( xt ? )
+  drop
+  state @ IF ['] (lit) , , ( ) THEN
+; IMMEDIATE
+
+: DEFER! ( xt2 xt1 -- ) >body 4 cells + ! ;
+: DEFER@ ( xt1 -- xt2 ) >body 4 cells + @ ;
+
+: IS ( xt "<spaces>name" -- )
+  bl word find ( xt ? )
+  0= IF ." Could not find word: " count type cr EXIT THEN
+  state @ IF ['] (lit) , ,  ['] defer! , ELSE defer! THEN
+; IMMEDIATE
+
+
+: AGAIN ( C: dest -- ) ['] (branch) , here - , ; IMMEDIATE
 
 \ Unimplemented output: .R
 \ Unimplemented double-cell: 2>R 2R> 2R@
