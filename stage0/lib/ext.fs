43c918d2bbfe551805ea7ac777f04f9c609e6d7d
Braden Shepherdson <braden.shepherdson@gmail.com>
2015-08-01 09:35:34 -0400

    <> and ?DO

--- stage0/lib/ext.fs
+++ stage0/lib/ext.fs
@@ -2,8 +2,18 @@
 
 : .(   [char] ) parse type ; IMMEDIATE
 
-: 0<> 0= INVERT ;
+: <> = INVERT ;
+: 0<> 0 <> ;
 : 0> 0 > ;
+
+
+: ?DO ( limit index --   C: old-jump-addr )
+  ['] 2dup , ['] swap , ['] >r dup , ,
+  ['] <> , ['] (0branch) ,
+  (loop-top) @    here (loop-top) ! ( C: old-jump-addr )
+  0 , \ Placeholder for the jump offset to go.
+; IMMEDIATE
+
 
 \ Unimplemented output: .R
 \ Unimplemented double-cell: 2>R 2R> 2R@
